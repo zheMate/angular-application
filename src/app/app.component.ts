9e90ce4d802fa185116f84021a7d0bb5e5689a69
Вуколов Данил Александрович <vukolov.da@pit.local>
2023-04-07 10:43:39 +0500

    get and edit model

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { NgModel } from '@angular/forms';
 
 
 export class Phone {
-    constructor(public title: string,
+    constructor(
+        public title: string,
         public price: number,
         public company: string)
-        {}
+    { }
 }
 
 class Item {
@@ -28,16 +30,21 @@ export class AppComponent {
     title: string = "";
     price: number = 0;
     company: string = "";
-    phone: Phone = new Phone("", 0, "Huawei");
-    phones: Phone[] = [];
+    phone: Phone = new Phone("", 0, "");
     companies: string[] = ["Apple", "Huawei", "Xiaomi", "Samsung", "LG", "Motorola", "Alcatel"];
-    addPhone(){
-        this.phones.push(new Phone(this.phone.title, this.phone.price, this.phone.company));
+    addPhone(title:NgModel, price: NgModel, comp: NgModel) {
+        console.log(title);
+        console.log(price);
+        console.log(comp);
+    }
+    onTitleChange(){
+        if(this.phone.title == "нет")
+        this.phone.title = "неизвестно";
     }
     text: string = "";
-    clicks:number = 0;
-    onChanged(increased:any){
-        increased == true?this.clicks++:this.clicks--;
+    clicks: number = 0;
+    onChanged(increased: any) {
+        increased == true ? this.clicks++ : this.clicks--;
     }
     age: number = 24;
     name: string = "Вася";
